053aa3c950f686f09e3bd32755b935150bb0f2f1
Dylan McGee <46721669+dtmcgee-su@users.noreply.github.com>
2025-11-17 12:29:56 -0500

    created senior thesis page

--- pages/senior_thesis.py
+++ pages/senior_thesis.py
@@ -1,1 +1,30 @@
 import streamlit as st
+
+st.title('Senior Thesis')
+
+with open('media/pdf/dylan_mcgee_senior_thesis_literature_review.pdf', "rb") as f:
+    pdf_data = f.read()
+
+st.download_button(
+    label="📄 Download Literature Review",
+    data=pdf_data,
+    file_name="media/pdf/dylan_mcgee_senior_thesis_literature_review.pdf",
+    mime="application/pdf"
+)
+
+with open('media/pdf/dylan_mcgee_senior_thesis_poster.pdf', "rb") as f:
+    pdf_data = f.read()
+
+st.download_button(
+    label="📄 Download PDF",
+    data=pdf_data,
+    file_name="media/pdf/dylan_mcgee_senior_thesis_poster.pdf",
+    mime="application/pdf"
+)
+
+
+
+
+
+st.image('./media/images/dylan_mcgee_senior_thesis.png',  use_container_width="always")
+
